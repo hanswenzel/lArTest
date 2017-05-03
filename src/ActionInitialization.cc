a2edf205527486a92c05f1294f79e170ed83ea6c
Geant4 Performance <g4p@tev.fnal.gov>
2017-05-03 18:16:19 -0500

    add the user ActionInitialization class for multithreading

---
+++ src/ActionInitialization.cc
@@ -0,0 +1,29 @@
+#include "ActionInitialization.hh"
+#include "PrimaryGeneratorAction.hh"
+#include "RunAction.hh"
+#include "EventAction.hh"
+#include "SteppingAction.hh"
+#include "DetectorConstruction.hh"
+
+ActionInitialization::ActionInitialization
+                      (DetectorConstruction* detConstruction)
+ : G4VUserActionInitialization(),
+   fDetConstruction(detConstruction)
+{}
+
+ActionInitialization::~ActionInitialization()
+{}
+
+void ActionInitialization::BuildForMaster() const
+{
+  SetUserAction(new RunAction);
+}
+
+void ActionInitialization::Build() const
+{
+  SetUserAction(new PrimaryGeneratorAction);
+  SetUserAction(new RunAction);
+  EventAction* eventAction = new EventAction;
+  SetUserAction(eventAction);
+  SetUserAction(new SteppingAction);
+}  
